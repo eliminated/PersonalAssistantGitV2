5539eae9b3544b3a01de30aa01a68fe519f9250d
Golden Smoothy <41362406+eliminated@users.noreply.github.com>
2022-05-12 21:59:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+
+const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const prefix = '-!';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const CommandFolders = fs.readdirSync("./commands");
+for (const folder of CommandFolders) {
+  const commandFiles = fs
+    .readdirSync(`./commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for(const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
+  }
+}
+
+client.on('ready', () => {
+  console.log(`Logged as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', msg => {
+  const args = msg.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (command === 'ping'){
+    msg.reply('pong!');
+  }
+})
+client.login(process.env.DISCORD_TOKEN);
